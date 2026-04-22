77e394a922b66ee05d19f496ebe1979330b99b22
Kyneb98 <tkwa22@student.aau.dk>
2026-04-22 10:35:56 +0200

    Challenge struktur og filer

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 68, 170, 198)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -89,34 +89,43 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    SizedBox(
+      width: 200,
+      height: 50,
+      child: FilledButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Text('Start'),
       ),
+    ),
+
+    const SizedBox(height: 16), // spacing between buttons
+
+    SizedBox(
+      width: 200,
+      height: 50,
+      child: FilledButton(
+        onPressed: () {},
+        child: const Text('Stop'),
+      ),
+    ),
+
+    const SizedBox(height: 16),
+
+    SizedBox(
+      width: 200,
+      height: 50,
+      child: FilledButton(
+        onPressed: () {},
+        child: const Text('Reset'),
+      ),
+    ),
+  ],
+)
+,
+      ),
+      
     );
   }
 }
