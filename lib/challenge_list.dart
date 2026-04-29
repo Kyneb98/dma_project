448ca3e495e153d3dd19fc48c815031ba2621b45
Kristoffer Mølgaard <kristof.moelgaard@gmail.com>
2026-04-29 09:19:04 +0200

    Added shake challenge

--- lib/challenge_list.dart
+++ lib/challenge_list.dart
@@ -17,18 +17,27 @@ import 'challenges/flip_right_challenge.dart';
 typedef Challenge = Future<void> Function();
 
 //en liste over alle challenges, som kan bruges til af vælge en random challenge fra
-
+final List<Challenge> challenges = [
+  
+  // Tilføj flere her
+];
 
 // Random generator
 final Random _rand = Random();
 
-
+// Funktion der returnerer en tilfældig challenge fra challengelisten
+Challenge getRandomChallenge() {
+  return challenges[_rand.nextInt(challenges.length)];
+}
 
 // sekvensen som kører de 7 challenges
 Future<void> runChallengeSequence(BuildContext context) async {
   final List<Widget> challengeWidgets = [
     const FlipDownChallenge(),
-    const FlipUpChallenge()
+    const FlipUpChallenge(),
+    const FlipLeftChallenge(),
+    const FlipRightChallenge(),
+    const ShakeChallenge(),
     // ... etc
   ];
   for (int i = 0; i < 7; i++) {
